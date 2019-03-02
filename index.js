a97ae9b6aa540b6c88a0a7239ab4cbe90883a978
Greg Bergé <greg@smooth-code.com>
2019-03-02 09:08:42 +0100

    feat: add react hooks & adjust rules
    BREAKING CHANGE: eslint-plugin-react-hooks is required

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
     'import/prefer-default-export': 'off',
+    'import/first': 'error',
+    'import/no-amd': 'error',
+    'import/no-webpack-loader-syntax': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.stories.js', '**/*.test.js'] },
+    ],
 
     'no-param-reassign': 'off',
     'no-shadow': 'off',
--- react.js
+++ react.js
@@ -2,14 +2,41 @@ const baseRules = require('./index').rules
 
 module.exports = {
   extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react-hooks'],
   rules: Object.assign(baseRules, {
+    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
     'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
     'react/prop-types': 'off',
     'react/sort-comp': 'off',
     'react/no-did-mount-set-state': 'off',
     'react/require-default-props': 'off',
     'react/no-array-index-key': 'off',
-    'react/no-typos': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/forbid-foreign-prop-types': ['warn', { allowInPropTypes: true }],
+    'react/jsx-no-comment-textnodes': 'warn',
+    'react/jsx-no-duplicate-props': ['warn', { ignoreCase: true }],
+    'react/jsx-no-target-blank': 'warn',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-pascal-case': [
+      'warn',
+      {
+        allowAllCaps: true,
+        ignore: [],
+      },
+    ],
+    'react/jsx-uses-react': 'warn',
+    'react/jsx-uses-vars': 'warn',
+    'react/no-danger-with-children': 'warn',
+    'react/no-deprecated': 'warn',
+    'react/no-direct-mutation-state': 'warn',
+    'react/no-is-mounted': 'warn',
+    'react/no-typos': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react/require-render-return': 'error',
+    'react/style-prop-object': 'warn',
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
 
     'jsx-a11y/anchor-is-valid': 'off',
     'jsx-a11y/anchor-has-content': 'off',
